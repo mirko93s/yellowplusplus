4dcaadffa10a06ff8cf9efd531c107bdf054056a
mirko93s <mirko93s@chill.ovh>
2023-02-06 11:38:31 +0100

    more wild pokemon slots

--- data/wild/probabilities.asm
+++ data/wild/probabilities.asm
@@ -3,13 +3,15 @@ WildMonEncounterSlotChances:
 ; those 10 slots is. A random number is generated and then the first byte of each pair in this
 ; table is compared against that random number. If the random number is less than or equal
 ; to the first byte, then that slot is chosen.  The second byte is double the slot number.
-	db  50, $00 ; 51/256 = 19.9% chance of slot 0
-	db 101, $02 ; 51/256 = 19.9% chance of slot 1
-	db 140, $04 ; 39/256 = 15.2% chance of slot 2
-	db 165, $06 ; 25/256 =  9.8% chance of slot 3
-	db 190, $08 ; 25/256 =  9.8% chance of slot 4
-	db 215, $0A ; 25/256 =  9.8% chance of slot 5
-	db 228, $0C ; 13/256 =  5.1% chance of slot 6
-	db 241, $0E ; 13/256 =  5.1% chance of slot 7
-	db 252, $10 ; 11/256 =  4.3% chance of slot 8
-	db 255, $12 ;  3/256 =  1.2% chance of slot 9
+	db  50, $00 ; 20%
+	db 101, $02 ; 20%
+	db 126, $04 ; 10%
+	db 151, $06 ; 10%
+	db 176, $08 ; 10%
+	db 201, $0A ; 10%
+	db 214, $0C ;  5%
+	db 227, $0E ;  5%
+	db 238, $10 ;  4%
+	db 249, $12 ;  4%
+	db 252, $14 ;  1%
+	db 255, $16 ;  1%
