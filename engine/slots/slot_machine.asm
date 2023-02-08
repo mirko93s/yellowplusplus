f78fab32cc92c0e7f2ff4cdf74840099be1eb638
mirko93s <mirko93s@chill.ovh>
2023-02-08 14:04:24 +0100

    og bug rigged slot machines
    The lucky slot machine in the Game Corner doesn't stop when it should if you get a 7.
    The lucky slot machine in the Game Corner doesn't stop when it should if there are two 7s or BARs on the middle or bottom of the wheel

--- engine/slots/slot_machine.asm
+++ engine/slots/slot_machine.asm
@@ -305,7 +305,7 @@ SlotMachine_StopWheel1Early:
 .loop
 	ld a, [hli]
 	cp HIGH(SLOTS7)
-	jr c, .stopWheel ; condition never true
+	jr z, .stopWheel
 	dec c
 	jr nz, .loop
 	ret
@@ -330,9 +330,13 @@ SlotMachine_StopWheel2Early:
 ; player's odds.
 .sevenAndBarMode
 	call SlotMachine_FindWheel1Wheel2Matches
+	ret nz
 	ld a, [de]
 	cp HIGH(SLOTSBAR) + 1
-	ret nc
+	jr c, .stopWheel
+	ld a, [wSlotMachineFlags]
+	bit 6, a
+	ret z
 .stopWheel
 	xor a
 	ld [wSlotMachineWheel2SlipCounter], a
