9a0db0f86c3ffb4c379b156f44e7376ff6c3b23c
mirko93s <mirko93s@chill.ovh>
2023-02-11 07:08:55 +0100

    fixed type in status screen and cleaner code

--- engine/battle/print_type.asm
+++ engine/battle/print_type.asm
@@ -21,6 +21,7 @@ PrintMonType:
 ; hl = dest addr
 PrintType:
 	push hl
+	ld hl, TypeNames
 	jr PrintType_
 
 ; erase "TYPE2/" if the mon only has 1 type
@@ -31,25 +32,15 @@ EraseType2Text:
 	ld bc, $6
 	jp FillMemory
 
-PrintMoveType:
+PrintMoveType: ; only used in battle
 	call GetPredefRegisters
 	push hl
 	ld a, [wPlayerMoveType]
+	ld hl, TypeNamesShort
 ; fall through
 
 PrintType_:
 	add a
-	ld hl, TypeNames
-	push af
-	ld a, [wIsInBattle]
-	cp 2
-	jr z, .shortNames  ; if trianer battle go to short
-	cp 1
-	jr nz, .regularNames ; else if wild fall through, else if not wild  go to regular
-.shortNames
-	ld hl, TypeNamesShort
-.regularNames
-	pop af
 	ld e, a
 	ld d, $0
 	add hl, de
