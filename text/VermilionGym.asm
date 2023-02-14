30d65b9bf3badc02263e87bb0dbaf18e5afc7dfd
mirko93s <mirko93s@chill.ovh>
2023-02-14 19:27:27 +0100

    fixed badge boost incorrect texts

--- text/VermilionGym.asm
+++ text/VermilionGym.asm
@@ -35,7 +35,7 @@ _LTSurgePostBattleAdviceText::
 _LTSurgeThunderBadgeInfoText::
 	text "The THUNDERBADGE"
 	line "cranks up your"
-	cont "#MON's SPEED!"
+	cont "#MON's DEFENSE!"
 
 	para "It also lets your"
 	line "#MON FLY any-"
