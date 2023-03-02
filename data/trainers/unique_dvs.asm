da1cccaaed1ebb93fd3338a7f42e53b70fc5e25e
mirko93s <mirko93s@chill.ovh>
2023-03-02 10:55:45 +0100

    few trainers got shiny mons
    a.j., joe and giselle

--- data/trainers/unique_dvs.asm
+++ data/trainers/unique_dvs.asm
@@ -4,6 +4,10 @@ UniqueDVTrainerPokemon:
 	; shiny
 	; db BROCK, 1, AERODACTYL, 12, $AA, $AA
 
+	db YOUNGSTER, 9, SANDSHREW, 33, $AA, $AA ; A.J.
+	db JR_TRAINER_M, 6, WEEPINBELL, 24, $AA, $AA ; JOE
+	db JR_TRAINER_F, 2, CUBONE, 25, $AA, $AA ; GISELLE
+
 	;other
 	; db LANCE, 1, DRAGONITE, 62, $FF, $FF
 
