mirko93s/yellowplusplus

commit d5bcf935aadedabceda0ddd520c3be31e0385aa1

og bug don't mark pokemon behind the ghost as seen in the pokedex

engine/battle/core.asm
@@ -6484,12 +6484,15 @@ LoadEnemyMonData:
 	ld a, [wEnemyMonSpecies2]
 	ld [wd11e], a
 	predef IndexToPokedex
+	call IsGhostBattle
+	jr nz, .noMarkSeen
 	ld a, [wd11e]
 	dec a
 	ld c, a
 	ld b, FLAG_SET
 	ld hl, wPokedexSeen
 	predef FlagActionPredef ; mark this mon as seen in the pokedex
+.noMarkSeen
 	ld hl, wEnemyMonLevel
 	ld de, wEnemyMonUnmodifiedLevel
 	ld bc, 1 + NUM_STATS * 2

commit 268b63f9f5ca79fce879b30830cec21c9e1d0c5f

fixed pokdex not saving seen pokemon
bug introduced in https://github.com/mirko93s/yellowplusplus/commit/d5bcf935aadedabceda0ddd520c3be31e0385aa1

engine/battle/core.asm
@@ -6611,7 +6611,8 @@ LoadEnemyMonData:
 	ld a, [wEnemyMonSpecies2]
 	ld [wd11e], a
 	call IsGhostBattle
-	jr nz, .noMarkSeen
+	jr z, .noMarkSeen
+	ld a, [wd11e]
 	dec a
 	ld c, a
 	ld b, FLAG_SET